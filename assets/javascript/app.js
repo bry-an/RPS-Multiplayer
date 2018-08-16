d0d6cc5313c877759715054d8b644254718093bf
Bryan Yunis <bbyunis@gmail.com>
2018-08-15 20:21:45 -0600

    changed structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDXVbNWsuLCknQtzjCvGoFwpg5jMK299D4",
+    authDomain: "multiplayer-rps-a820d.firebaseapp.com",
+    databaseURL: "https://multiplayer-rps-a820d.firebaseio.com",
+    projectId: "multiplayer-rps-a820d",
+    storageBucket: "multiplayer-rps-a820d.appspot.com",
+    messagingSenderId: "947470144121"
+  };
+  firebase.initializeApp(config);
+  database = firebase.database();
